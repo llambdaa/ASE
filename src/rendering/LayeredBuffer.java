3e17da3b198639d26ac844ec1a6f5d0c3a271c83
Lukas Rapp <lu.rapp@web.de>
2023-03-04 13:51:20 +0100

    Added layered buffer for dynamic rendering

--- src/rendering/LayeredBuffer.java
+++ src/rendering/LayeredBuffer.java
@@ -1,2 +1,41 @@
-package rendering;public class LayeredBuffer {
+package rendering;
+
+import utils.Tuple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LayeredBuffer extends Buffer {
+    private List<Buffer> buffers;
+    private Map<Buffer, Tuple<Integer, Integer>> coordinates;
+    private char background;
+    
+    public LayeredBuffer(int width, int height, char background) {
+        super(width, height);
+        this.buffers = new ArrayList<>();
+        this.coordinates = new HashMap<>();
+        this.background = background;
+    }
+    
+    public LayeredBuffer(int width, int height) {
+        this(width, height, ' ');
+    }
+    
+    public void add(int x, int y, Buffer buffer) {
+        this.buffers.add(buffer);
+        this.coordinates.put(buffer, new Tuple<>(x, y));
+    }
+    
+    public void render() {
+        this.fill(this.background);
+        for (Buffer buffer : this.buffers) {
+            Tuple<Integer, Integer> coordinates = this.coordinates.get(buffer);
+            int x = coordinates._1;
+            int y = coordinates._2;
+            this.write(x, y, buffer);
+        }
+    }
+    
 }
--- src/utils/Tuple.java
+++ src/utils/Tuple.java
@@ -1,2 +1,11 @@
-package utils;public class Tuple {
+package utils;
+
+public class Tuple<A, B> {
+    public A _1;
+    public B _2;
+    
+    public Tuple(A first, B second) {
+        this._1 = first;
+        this._2 = second;
+    }
 }
