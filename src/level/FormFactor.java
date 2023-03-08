ecb6dba45f0dcfacc37cf2d4855f875c63861afa
Lukas Rapp <lu.rapp@web.de>
2023-03-08 15:52:16 +0100

    Replaced hard-coded door count by wall count

--- src/level/FormFactor.java
+++ src/level/FormFactor.java
@@ -1,30 +1,26 @@
 package level;
 
-import utils.IntRange;
-
 public enum FormFactor {
-    SMALL_SQUARE(1, 1, IntRange.from(1, 4)),
-    MEDIUM_SQUARE(2, 2, IntRange.from(1, 4)),
-    MEDIUM_LINE_X(2, 1, IntRange.from(1, 4)),
-    MEDIUM_LINE_Y(1, 2, IntRange.from(1, 4)),
-    LARGE_SQUARE(3, 3, IntRange.from(1, 8)),
-    LARGE_LINE_X(3, 1, IntRange.from(1, 6)),
-    LARGE_LINE_Y(1, 3, IntRange.from(1, 6)),
-    LARGE_DLINE_X(3, 2, IntRange.from(1, 6)),
-    LARGE_DLINE_Y(2, 3, IntRange.from(1, 6));
+    SMALL_SQUARE(1, 1),
+    MEDIUM_SQUARE(2, 2),
+    MEDIUM_LINE_X(2, 1),
+    MEDIUM_LINE_Y(1, 2),
+    LARGE_SQUARE(3, 3),
+    LARGE_LINE_X(3, 1),
+    LARGE_LINE_Y(1, 3),
+    LARGE_DLINE_X(3, 2),
+    LARGE_DLINE_Y(2, 3);
     
-    FormFactor(int horizontal, int vertical, IntRange doorCount) {
+    FormFactor(int horizontal, int vertical) {
         this.horizontal = horizontal;
         this.vertical = vertical;
-        this.doorCount = doorCount;
     }
     
-    private static final int SMALL_ROOM_WIDTH = 10;
-    private static final int SMALL_ROOM_HEIGHT = 10;
+    private static final int SMALL_ROOM_WIDTH = 11;
+    private static final int SMALL_ROOM_HEIGHT = 11;
     
     private int horizontal;
     private int vertical;
-    private IntRange doorCount;
     
     public int getScaledWidth() {
         return this.horizontal * SMALL_ROOM_WIDTH;
@@ -34,8 +30,8 @@ public enum FormFactor {
         return this.vertical * SMALL_ROOM_HEIGHT;
     }
     
-    public IntRange getDoorCountRange() {
-        return this.doorCount;
+    public int getWallCount() {
+        return 2 * (this.horizontal + this.vertical);
     }
     
 }
