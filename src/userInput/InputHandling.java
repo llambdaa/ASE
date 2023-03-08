126a47ed57e5b383727abd8172321063af636601
uHirsch <66682042+Swr96@users.noreply.github.com>
2023-03-08 20:09:26 +0100

    implemented input handling for movement, no logic

--- src/userInput/InputHandling.java
+++ src/userInput/InputHandling.java
@@ -1,6 +1,26 @@
 package userInput;
 
-public class KeyBindings {
+import java.util.HashMap;
+import java.util.function.Function;
 
-    
+public class InputHandling {
+    protected  HashMap<Integer, Runnable> keyHandling = new HashMap<>();
+     public InputHandling(){
+        keyHandling.put(87, () -> {
+
+        });
+        keyHandling.put(65, () -> {
+            //a
+
+        });
+        keyHandling.put(83, () -> {
+            //s
+
+        });
+        keyHandling.put(68, () -> {
+            //d
+
+        });
+    }
+
 }
--- src/userInput/KeyListen.java
+++ src/userInput/KeyListen.java
@@ -3,13 +3,13 @@ package userInput;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class KeyListen implements KeyListener {
+public class KeyListen extends InputHandling implements KeyListener {
     public KeyListen(){
 
     }
     @Override
     public void keyPressed(KeyEvent e) {
-
+        this.keyHandling.get(e.getKeyCode()).run();
     }
 
     @Override
