4647921b3b26dc6dd8a6078245660390670a60a9
uHirsch <66682042+Swr96@users.noreply.github.com>
2023-03-10 14:53:37 +0100

    merged input handling in one file

--- src/userInput/InputHandling.java
+++ src/userInput/InputHandling.java
@@ -1,26 +1,51 @@
 package userInput;
 
+import domain.Player;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.HashMap;
-import java.util.function.Function;
 
-public class InputHandling {
-    protected  HashMap<Integer, Runnable> keyHandling = new HashMap<>();
-     public InputHandling(){
-        keyHandling.put(87, () -> {
+public class InputHandling implements KeyListener {
+    protected HashMap<Integer, Runnable> keyHandling = new HashMap<>();
+    private Player player;
 
+    public InputHandling(Player player) {
+        this.player = player;
+        registerKeys();
+    }
+
+    private void registerKeys(){
+        keyHandling.put(KeyEvent.VK_W, () -> {
+            //w
+            System.out.println("w");
         });
-        keyHandling.put(65, () -> {
+        keyHandling.put(KeyEvent.VK_A, () -> {
             //a
 
         });
-        keyHandling.put(83, () -> {
+        keyHandling.put(KeyEvent.VK_S, () -> {
             //s
 
         });
-        keyHandling.put(68, () -> {
+        keyHandling.put(KeyEvent.VK_D, () -> {
             //d
 
         });
     }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyCode());
+        this.keyHandling.get(e.getKeyCode()).run();
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
