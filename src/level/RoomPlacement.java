8a92332e1fc547638ad377cc26a8e80e4f6489ba
Lukas Rapp <lu.rapp@web.de>
2023-03-13 00:31:33 +0100

    Added RoomPlacement.small helper function for initial room layout generation

--- src/level/RoomPlacement.java
+++ src/level/RoomPlacement.java
@@ -1,15 +1,28 @@
 package level;
 
-public record RoomPlacement(GridPosition start, FormFactor form) {
+public record RoomPlacement(GridPosition position, FormFactor form) {
     public RoomPlacement(int x, int y, FormFactor form) {
         this(new GridPosition(x, y), form);
     }
     
+    public static RoomPlacement small(GridPosition position) {
+        return RoomPlacement.small(position.x(), position.y());
+    }
+    
+    public static RoomPlacement small(int x, int y) {
+        return new RoomPlacement(x, y, FormFactor.SMALL_SQUARE);
+    }
+    
+    public GridPosition position() {
+        return this.position;
+    }
+    
     public int x() {
-        return this.start.x();
+        return this.position.x();
     }
     
     public int y() {
-        return this.start.y();
+        return this.position.y();
     }
+    
 }
