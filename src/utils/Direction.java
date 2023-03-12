16ab7f1644669baf91f662de5cc0707107641490
Lukas Rapp <lu.rapp@web.de>
2023-03-13 00:30:33 +0100

    Revert Direction to simple enum

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -1,23 +1,9 @@
 package utils;
 
 public enum Direction {
-    LEFT(true),
-    RIGHT(true),
-    UP(false),
-    DOWN(false);
-    
-    Direction(boolean horizontal) {
-        this.horizontal = horizontal;
-    }
-    
-    private boolean horizontal;
-    
-    public boolean isHorizontal() {
-        return this.horizontal;
-    }
-    
-    public boolean isVertical() {
-        return !this.horizontal;
-    }
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN;
     
 }
