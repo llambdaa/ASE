94ae8f31cb61183d78ebed70b2ce9bfb33638926
Lukas Rapp <lu.rapp@web.de>
2023-03-12 02:52:18 +0100

    Added rudimentary level generation

--- src/level/LevelGenerator.java
+++ src/level/LevelGenerator.java
@@ -4,19 +4,93 @@ import utils.Direction;
 import utils.IntRange;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class LevelGenerator {
-    private static final int MAX_ROOM_COUNT = 30;
+    private static final int MAX_ROOM_COUNT = 10;
     private static final int GRID_WIDTH = 10;
     private static final int GRID_HEIGHT = 10;
     public static final RoomGrid GRID = new RoomGrid(GRID_WIDTH, GRID_HEIGHT);
     
     public static Level generate() {
-        return null;
+        List<Room> generatedRooms = new ArrayList<>();
+        List<Room> untouchedRooms = new ArrayList<>();
+        int wantedRoomCount = IntRange.from(2, MAX_ROOM_COUNT).random();
+        int generatedRoomCount = 0;
+        
+        // 1) Generate spawn room
+        Room spawn = GRID.generateSpawnRoom();
+        untouchedRooms.add(spawn);
+        generatedRoomCount += 1;
+        
+        // 2) Generate neighboring rooms
+        generation:
+        while (!untouchedRooms.isEmpty() && generatedRoomCount < wantedRoomCount) {
+            Room seed = untouchedRooms.remove(0);
+            int walls = seed.getFormFactor().getWallCount();
+            int missingRooms = wantedRoomCount - generatedRoomCount;
+            int doors = IntRange.from(1, Math.max(missingRooms, walls)).random();
+            
+            List<DoorPlacement> vacant = findVacantDoorPlacements(seed);
+            Collections.shuffle(vacant);
+            
+            for (int d = 0; d < doors; d++) {
+                Iterator<DoorPlacement> it = vacant.iterator();
+                while (it.hasNext()) {
+                    DoorPlacement candidate = it.next();
+                    GridPosition position = candidate.position();
+                    Direction facing = candidate.facing();
+                    GridPosition target = position.getAdjacentPosition(facing);
+                    
+                    // The door points onto an empty grid position.
+                    // Another room is generated there.
+                    if (GRID.isFree(target, FormFactor.SMALL_SQUARE)) {
+                        Room generated = generateRoom(candidate);
+                        untouchedRooms.add(generated);
+                        generatedRoomCount++;
+                        
+                        if (generatedRoomCount >= wantedRoomCount) {
+                            break generation;
+                        }
+                    }
+                    
+                    it.remove();
+                }
+            }
+            
+            generatedRooms.add(seed);
+        }
+        
+        generatedRooms.addAll(untouchedRooms);
+        Level result = new Level(generatedRooms);
+        return result;
     }
     
-    public static RoomPlacement selectRoomPlacement(int x, int y, Direction direction) {
+    private static Room generateRoom(DoorPlacement door) {
+        GridPosition position = door.position();
+        RoomPlacement placement = selectRoomPlacement(position.x(), position.y(), door.facing());
+        GRID.place(placement);
+        
+        // TODO # CONNECT BOTH ROOMS
+        // TODO # AND LET DOORS DRAW
+        // TODO # WITH OFFSET
+        
+        
+        return new Room(placement);
+    }
+    
+    private static List<DoorPlacement> findVacantDoorPlacements(Room room) {
+        List<DoorPlacement> placements = room.getFormFactor().getDoorPlacements(room.getGridStart());
+        for (Door inherited : room.getDoors()) {
+            placements.remove(inherited.getPlacement());
+        }
+        
+        return placements;
+    }
+    
+    private static RoomPlacement selectRoomPlacement(int x, int y, Direction direction) {
         List<RoomPlacement> placements = findValidRoomPlacements(x, y, direction);
         
         // Each placement is represented by multiple duplicate indices to it,
