6a6d099b16b12c902ff0ca9eda44b35c3ed5c4ee
Lukas Rapp <lu.rapp@web.de>
2023-03-12 02:51:58 +0100

    Added generator for door placements based on form factor

--- src/level/FormFactor.java
+++ src/level/FormFactor.java
@@ -9,15 +9,15 @@ import java.util.List;
 import java.util.Map;
 
 public enum FormFactor {
-    SMALL_SQUARE(1, 1, 8),
+    SMALL_SQUARE(1, 1, 5),
     MEDIUM_SQUARE(2, 2, 2),
-    MEDIUM_LINE_X(2, 1, 2),
-    MEDIUM_LINE_Y(1, 2, 2),
+    MEDIUM_LINE_X(2, 1, 6),
+    MEDIUM_LINE_Y(1, 2, 6),
     LARGE_SQUARE(3, 3, 1),
-    LARGE_LINE_X(3, 1, 1),
-    LARGE_LINE_Y(1, 3, 1),
-    LARGE_DLINE_X(3, 2, 1),
-    LARGE_DLINE_Y(2, 3, 1);
+    LARGE_LINE_X(3, 1, 3),
+    LARGE_LINE_Y(1, 3, 3),
+    LARGE_DLINE_X(3, 2, 2),
+    LARGE_DLINE_Y(2, 3, 2);
     
     FormFactor(int horizontal, int vertical, int preference) {
         this.horizontal = horizontal;
@@ -28,7 +28,7 @@ public enum FormFactor {
     private static final int SMALL_ROOM_WIDTH = 11;
     private static final int SMALL_ROOM_HEIGHT = 11;
     public static final int DOOR_DEVIATION = 1;
-    public static final Map<FormFactor, List<Tuple<Direction, Integer>>> DOOR_LOCATIONS = new HashMap<>();
+    public static final Map<FormFactor, List<Tuple<Direction, Integer>>> BUFFER_DOOR_LOCATIONS = new HashMap<>();
     
     static {
         int halfRoomWidth = FormFactor.SMALL_ROOM_WIDTH / 2;
@@ -49,7 +49,7 @@ public enum FormFactor {
                 locations.add(new Tuple<>(Direction.DOWN, offset));
             }
             
-            FormFactor.DOOR_LOCATIONS.put(form, locations);
+            FormFactor.BUFFER_DOOR_LOCATIONS.put(form, locations);
         }
     }
     
@@ -79,6 +79,39 @@ public enum FormFactor {
     
     public int getWallCount() {
         return 2 * (this.horizontal + this.vertical);
+    }
+    
+    public List<DoorPlacement> getDoorPlacements(GridPosition start) {
+        List<DoorPlacement> adjacent = new ArrayList<>();
+        int x, y;
+        
+        for (int h = 0; h < this.vertical; h++) {
+            y = start.y() + h;
+            
+            for (int w = 0; w < this.horizontal; w++) {
+                x = start.x() + w;
+                if (w == 0) {
+                    // Left-most position
+                    adjacent.add(new DoorPlacement(x, y, Direction.LEFT));
+                }
+                
+                if (w == this.horizontal - 1) {
+                    // Right-most position
+                    adjacent.add(new DoorPlacement(x, y, Direction.RIGHT));
+                }
+                
+                if (h == 0) {
+                    // Top-most position
+                    adjacent.add(new DoorPlacement(x, y, Direction.UP));
+                }
+                
+                if (h == this.vertical - 1) {
+                    // Bottom-most position
+                    adjacent.add(new DoorPlacement(x, y, Direction.DOWN));
+                }
+            }
+        }
+        return adjacent;
     }
     
 }
