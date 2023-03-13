029ce803f368c359d83a442775299af68c4cd02d
Lukas Rapp <lu.rapp@web.de>
2023-03-13 17:33:11 +0100

    Added more boundary information and checks to RoomGrid

--- src/level/RoomGrid.java
+++ src/level/RoomGrid.java
@@ -7,6 +7,10 @@ public class RoomGrid {
     private int height;
     private int horizontalOffset;
     private int verticalOffset;
+    private int xMin;
+    private int xMax;
+    private int yMin;
+    private int yMax;
     private boolean[][] grid;
     
     public RoomGrid(int width, int height) {
@@ -14,18 +18,55 @@ public class RoomGrid {
         this.height = height;
         this.horizontalOffset = width / 2;
         this.verticalOffset = height / 2;
+        
+        this.xMin = -this.horizontalOffset;
+        this.yMin = -this.verticalOffset;
+        this.xMax = this.horizontalOffset;
+        this.yMax = this.verticalOffset;
+        
+        if (this.width % 2 == 0) {
+            this.xMax -= 1;
+        }
+        
+        if (this.height % 2 == 0) {
+            this.yMax -= 1;
+        }
+        
         this.grid = new boolean[height][width];
     }
     
-    public boolean isFree(GridPosition position) {
-        int x = position.x() + horizontalOffset;
-        int y = position.y() + verticalOffset;
-        
+    public int getMinX() {
+        return this.xMin;
+    }
+    
+    public int getMaxX() {
+        return this.xMax;
+    }
+    
+    public int getMinY() {
+        return this.yMin;
+    }
+    
+    public int getMaxY() {
+        return this.yMax;
+    }
+    
+    public boolean isInside(int x, int y) {
         if (x < 0 || x >= this.width) {
             return false;
         }
         
         if (y < 0 || y >= this.height) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean isFree(GridPosition position) {
+        int x = position.x() + this.horizontalOffset;
+        int y = position.y() + this.verticalOffset;
+        if (!isInside(x, y)) {
             return false;
         }
         
@@ -54,9 +95,48 @@ public class RoomGrid {
     }
     
     public void reserve(GridPosition position) {
-        int x = position.x() + horizontalOffset;
-        int y = position.y() + verticalOffset;
+        int x = position.x() + this.horizontalOffset;
+        int y = position.y() + this.verticalOffset;
+        if (!isInside(x, y)) {
+            return;
+        }
+        
         this.grid[y][x] = true;
+    }
+    
+    public void reserve(RoomPlacement placement) {
+        int xMin = placement.x() + this.horizontalOffset;
+        int xMax = xMin + placement.form().getHorizontalScale();
+        int yMin = placement.y() + this.verticalOffset;
+        int yMax = yMin + placement.form().getVerticalScale();
+        
+        for (int h = yMin; h < yMax; h++) {
+            for (int w = xMin; w < xMax; w++) {
+                this.grid[h][w] = true;
+            }
+        }
+    }
+    
+    public void free(GridPosition position) {
+        int x = position.x() + this.horizontalOffset;
+        int y = position.y() + this.verticalOffset;
+        
+        if (isInside(x, y)) {
+            this.grid[y][x] = false;
+        }
+    }
+    
+    public void free(RoomPlacement placement) {
+        int xMin = placement.x() + this.horizontalOffset;
+        int xMax = xMin + placement.form().getHorizontalScale();
+        int yMin = placement.y() + this.verticalOffset;
+        int yMax = yMin + placement.form().getVerticalScale();
+        
+        for (int h = yMin; h < yMax; h++) {
+            for (int w = xMin; w < xMax; w++) {
+                this.grid[h][w] = false;
+            }
+        }
     }
     
     public boolean fits(RoomPlacement placement) {
@@ -67,7 +147,7 @@ public class RoomGrid {
         
         for (int h = yMin; h < yMax; h++) {
             for (int w = xMin; w < xMax; w++) {
-                if (!this.grid[h][w]) {
+                if (!isInside(w, h) || !this.grid[h][w]) {
                     return false;
                 }
             }
